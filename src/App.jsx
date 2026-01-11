eb32a22c121b8ae02fd7c958e009265275bae06a
Happiness-x <peaceofmind.happinessx@gmail.com>
2026-01-11 17:53:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -346,7 +346,7 @@ export default function App() {
       </a>
 
       <a
-        href="https://wa.me/91877845559"
+        href="https://wa.me/918777845559"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 p-4 rounded-full shadow-lg hover:bg-green-600 transition z-50"
