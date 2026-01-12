adbe14ab7c525a35751cc2d8a1edb4ca7c9ce014
Happiness-x <peaceofmind.happinessx@gmail.com>
2026-01-12 13:08:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,37 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 import Header from "./components/Header";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import QuantumHealing from "./pages/QuantumHealing";
-import LearnMore from "./pages/LearnMore";
 import Gallery from "./pages/Gallery";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
+import LearnMore from "./pages/LearnMore";
 
 export default function App() {
   useEffect(() => {
-    AOS.init({ duration: 1200, once: true, easing: "ease-out-cubic" });
+    AOS.init({ duration: 1200, once: true });
   }, []);
 
   return (
-    <div className="relative bg-gradient-to-b from-black via-teal-900 to-black text-white font-serif scroll-smooth">
+    <div className="bg-black text-white font-serif min-h-screen">
+      {/* Fixed Header */}
       <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/quantum-healing" element={<QuantumHealing />} />
-        <Route path="/learn" element={<LearnMore />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogPost />} />
-      </Routes>
+      {/* MAIN CONTENT OFFSET */}
+      <main className="pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/quantum-healing" element={<QuantumHealing />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path="/learn" element={<LearnMore />} />
+        </Routes>
+      </main>
     </div>
   );
 }
